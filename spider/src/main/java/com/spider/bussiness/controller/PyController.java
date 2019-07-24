bf3b5acf4eeb2245eff01c10c3c9e473b8e9b71b
mengyya <mengyya@yonyou.com>
2019-07-24 22:40:34 +0800

    1

--- spider/src/main/java/com/spider/bussiness/controller/PyController.java
+++ spider/src/main/java/com/spider/bussiness/controller/PyController.java
@@ -4,6 +4,7 @@ import com.common.bussiness.controller.BaseController;
 import com.common.pub.pubBo.Resp;
 import com.spider.bussiness.service.ESService;
 import com.spider.pub.consts.Consts;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,33 +24,40 @@ public class PyController extends BaseController {
     @Autowired
     private ESService esService;
 
+    // private static String pyPath = "python D:\\Dev\\WorkSpace\\pythonWorkspace\\collect\\spider\\sina_main.py ";
+    private static String pyPath = "python E:\\Yonyou\\WorkSpace\\pythonWorkspace\\spider\\spider\\sina_main.py ";
+
     @RequestMapping("/exeSearch")
     @ResponseBody
     public Resp<String> exeSearch(String searchUser) {
         try {
-//             Process process = Runtime.getRuntime().exec("python D:\\Dev\\WorkSpace\\pythonWorkspace\\collect\\spider\\sina_main.py " + searchUser);
-//             try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()))){
-//                 String line;
-//                 while ((line = bufferedReader.readLine()) != null){
-//                     if(!line.matches(".*<.*") && !line.matches(".*</")
-//                             && !line.matches(".*\\(") && !line.matches(".*: ")
-//                             && !line.matches(".*;")){
-//                        System.out.println(line + "\n");
-//                        rabbitTemplate.convertAndSend("FanoutExchange1","",line, new CorrelationData(UUID.randomUUID().toString()));
-//                     }
-//                 }
-//             }
-//             System.out.println("\n\n-----------------------------------------------------------------\n");
-//             try (BufferedReader errorBufferedReader = new BufferedReader(new InputStreamReader(process.getErrorStream()))){
-//                 String line;
-//                 while ((line = errorBufferedReader.readLine()) != null){
-//                     System.out.println(line + "\n");
-//                 }
-//             }
-//             process.waitFor();
+            Process process = Runtime.getRuntime().exec(pyPath + searchUser);
+            try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()))){
+                String line;
+                while ((line = bufferedReader.readLine()) != null){
+                    if(StringUtils.isNotBlank(line)){
+                        line = line.replaceAll(".*\\w.*\\d.*", "").replaceAll("\\w", "")
+                            .replaceAll("[`#$%^&*()_\\-+=<>:\"{}|,.\\/;'\\[\\]·~！@#￥%……&*（）——\\-+={}|《》【】]", "");
+                        if(StringUtils.isNotBlank(line)) {
+                            System.out.println(line + "\n");
+                            rabbitTemplate.convertAndSend("FanoutExchange1","",line, new CorrelationData(UUID.randomUUID().toString()));
+                        }
+                    }
+                }
+
+                rabbitTemplate.convertAndSend("FanoutExchange1","","搜索完毕", new CorrelationData(UUID.randomUUID().toString()));
+            }
+            System.out.println("\n\n-----------------------------------------------------------------\n");
+            try (BufferedReader errorBufferedReader = new BufferedReader(new InputStreamReader(process.getErrorStream()))){
+                String line;
+                while ((line = errorBufferedReader.readLine()) != null){
+                    System.out.println(line + "\n");
+                }
+            }
+            process.waitFor();
 
             esService.deleteAllDoc(Consts.indexName);
-//            esService.addRelationDoc(Consts.indexName);
+            esService.addRelationDoc(Consts.indexName);
 
 //            for(int i = 0; i < 100; i++)
 //                rabbitTemplate.convertAndSend("FanoutExchange1","","aaaaaaaa", new CorrelationData(UUID.randomUUID().toString()));
